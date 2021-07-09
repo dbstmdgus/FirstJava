283d394546cf1b9810aa202134230ae72d29a43c
robot <robot@208-pro4-7>
2021-07-09 10:58:31 +0900

    첫번째 Git Test

---
+++ FirstGitJava/src/s20414_0709/GitJava.java
@@ -0,0 +1,10 @@
+package s20414_0709;
+
+public class GitJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("·Îº¿°í 2ÇÐ³â 4¹Ý 14¹ø À±½ÂÇöÀÔ´Ï´Ù.");
+	}
+
+}
