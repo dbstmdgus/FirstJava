bf5d9c060675d46a87bc3e9433cb4f9661e78b84
robot <robot@208-pro4-7>
2021-07-09 11:17:39 +0900

    두번째 소스 올리기

--- FirstGitJava/src/s20414_0709/GitJava.java
+++ FirstGitJava/src/s20414_0709/GitJava.java
@@ -5,6 +5,8 @@ public class GitJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("로봇고 2학년 4반 14번 윤승현입니다.");
+		
+		System.out.println("수정합니다");
 	}
 
 }
